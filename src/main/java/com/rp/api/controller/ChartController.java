a8df04f2adc6b2e21a47ec2c6f8b66e13d28f96a
Ramesh <rameshchandrapanda15@gmail.com>
2022-06-30 13:28:30 +0530

    first commit

---
+++ src/main/java/com/rp/api/controller/ChartController.java
@@ -0,0 +1,26 @@
+package com.rp.api.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.rp.api.model.ChatMessage;
+
+@Controller
+public class ChartController {
+
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor accessor) {
+		accessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+}
